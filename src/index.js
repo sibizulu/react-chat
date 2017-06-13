ed9bea7f31afe60622a8f3ce3aeb368467e79c00
sibizulu <sibiraj@lightrains.com>
2017-06-13 15:26:14 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import ChatBot from 'react-simple-chatbot'
+
+const steps = [
+  {
+    id: '0',
+    message: 'Welcome to react chatbot!',
+    trigger: '1'
+  },
+  {
+    id: '1',
+    message: 'Bye!',
+    end: true
+  }
+]
+
+ReactDOM.render(
+  <div>
+    <ChatBot steps={steps} />
+  </div>,
+  document.getElementById('root')
+)
